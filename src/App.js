2ff30dd6cef6c06986cec424182838009f93a00d
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-02-28 08:55:24 -0600

    Add a new font and cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import React from "react";
+import React from 'react'
+import ExpansionPanel from './ExpansionPanel'
 
 function App() {
   return (
-    <div className="md:flex m-4 p-2 shadow rounded-lg">
-      <div className="md:flex-shrink-0">
-        <img
-          className="rounded-lg md:w-56"
-          src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-          alt="Woman paying for a purchase"
-        />
-      </div>
-      <div className="mt-4 md:mt-0 md:ml-6">
-        <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
-          Marketing
-        </div>
-        <a
-          href="#"
-          className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
-        >
-          Finding customers for your new business
-        </a>
-        <p className="mt-2 text-gray-600">
-          Getting a new business off the ground is a lot of hard work. Here are
-          five ideas you can use to find your first customers.
-        </p>
-      </div>
-    </div>
-  );
+    <main className="antialiased text-gray-900 font-body">
+      <h1 className="m-4 text-3xl font-medium ">Messing with tailwind</h1>
+      <ExpansionPanel />
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['-apple-system', 'BlinkMacSystemFont'],
+      serif: ['Georgia', 'Cambria'],
+      mono: ['SFMono-Regular', 'Menlo'],
+      display: ['Oswald'],
+      body: ['Poppins']
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
