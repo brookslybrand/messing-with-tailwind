3d2a2b5d1e2c9c91d62324ec0a00c749a67185b9
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-02-29 13:07:03 -0600

    Add different gray color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
       body: ['Poppins', '-apple-system', 'BlinkMacSystemFont']
     },
     extend: {
+      colors: {
+        // taken from https://tailwindcss.com/docs/customizing-colors/#overriding-a-default-color
+        gray: {
+          '100': '#f5f5f5',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121'
+        }
+      },
       transitionProperty: {
         height: 'height'
       }
