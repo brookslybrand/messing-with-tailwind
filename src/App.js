74abef753b12fff7fc3b118f0ee3ff0785a1ae83
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-02-29 14:09:59 -0600

    Add `ExpansionPanelSummary` and `ExpansionPanelDetails`

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import ExpansionPanel from './ExpansionPanel'
+import {
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
+} from './ExpansionPanel'
 
 function App() {
   return (
-    <main className="antialiased text-gray-900 font-body">
-      <h1 className="m-4 text-3xl font-medium">Messing with tailwind</h1>
-      <ExpansionPanel />
+    <main className="antialiased text-gray-900 font-body flex flex-col items-center">
+      <h1 className="m-4 text-3xl font-medium self-start">
+        Messing with tailwind
+      </h1>
+      <div className="max-w-3xl w-full">
+        <ExpansionPanel>
+          <ExpansionPanelSummary>
+            <p className="my-3 text-lg">Expansion Panel 1</p>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </p>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
     </main>
   )
 }
--- src/ExpansionPanel.js
+++ src/ExpansionPanel.js
@@ -1,4 +1,10 @@
-import React, { useState } from 'react'
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useContext,
+  createContext
+} from 'react'
 
 import { MdExpandMore as ExpandMoreIcon } from 'react-icons/md'
 
@@ -10,7 +16,11 @@ import {
 } from '@reach/accordion'
 import '@reach/accordion/styles.css'
 
-export default function SimpleExpansionPanel() {
+const ExpansionContext = createContext()
+
+export { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails }
+
+function ExpansionPanel({ children }) {
   const [expanded, setExpanded] = useState(false)
 
   return (
@@ -20,35 +30,75 @@ export default function SimpleExpansionPanel() {
         index={expanded ? 0 : null}
         onChange={e => setExpanded(prev => !prev)}
       >
-        <AccordionItem>
-          <AccordionButton className="flex w-full items-center justify-between px-6 focus:bg-gray-300 focus:outline-none">
-            <p className="my-3 text-lg">Expansion Panel 1</p>
-
-            <div aria-disabled="false" aria-hidden="true">
-              <ExpandMoreIcon
-                // className="transition-transform ease-in-out"
-                className={`w-6 h-6 fill-current text-gray-600 transition duration-150 ease-in-out transform ${
-                  expanded ? 'rotate-180' : 'rotate-0'
-                }`}
-              />
-            </div>
-          </AccordionButton>
-          <hr />
-          <AccordionPanel
-            className={`block min-h-0 overflow-hidden transition-height duration-200 ease-in-out ${
-              expanded ? 'visible h-16' : 'invisible h-0'
-            }`}
-          >
-            <div className="visible p-6">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </p>
-            </div>
-          </AccordionPanel>
-        </AccordionItem>
+        <ExpansionContext.Provider value={expanded}>
+          {children}
+        </ExpansionContext.Provider>
       </Accordion>
     </div>
   )
+}
+
+function ExpansionPanelSummary({ children }) {
+  const expanded = useExpanded()
+  return (
+    <AccordionItem>
+      <AccordionButton className="flex w-full items-center justify-between px-6 focus:bg-gray-300 focus:outline-none">
+        {children}
+
+        <div aria-disabled="false" aria-hidden="true">
+          <ExpandMoreIcon
+            className={`w-6 h-6 fill-current text-gray-600 transition duration-150 ease-in-out transform ${
+              expanded ? 'rotate-180' : 'rotate-0'
+            }`}
+          />
+        </div>
+      </AccordionButton>
+      <hr />
+    </AccordionItem>
+  )
+}
+
+function ExpansionPanelDetails({ children }) {
+  const [ref, height] = useHeight()
+  const expanded = useExpanded()
+  return (
+    <AccordionPanel
+      className={`block min-h-0 overflow-hidden transition-height duration-200 ease-in-out ${
+        expanded ? 'visible' : 'invisible'
+      }`}
+      // have to manually add the style here
+      style={{ height: expanded ? height : 0 }}
+    >
+      <div ref={ref} className="visible p-6">
+        {children}
+      </div>
+    </AccordionPanel>
+  )
+}
+
+// hooks!
+
+const useExpanded = () => {
+  const expanded = useContext(ExpansionContext)
+  if (expanded === undefined)
+    throw new Error(
+      `useExpanded must be used in a component rendered as a decendent of ExpansionPanel`
+    )
+  return expanded
+}
+
+const useHeight = () => {
+  const ref = useRef()
+  const [height, setHeight] = useState()
+
+  useEffect(() => {
+    // create the resize callback and call it initially
+    const handleSetHeight = () => setHeight(ref.current.offsetHeight)
+    handleSetHeight()
+    // setup the event listener and tear it down when the component dismounts
+    window.addEventListener('resize', handleSetHeight)
+    return () => window.removeEventListener('resize', handleSetHeight)
+  }, [])
+
+  return [ref, height]
 }
